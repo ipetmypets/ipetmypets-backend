46dad3b7b3917480f6725bed3b18201f8738a951
BappiDeb <bappy08deb@gmail.com>
2025-01-03 06:14:23 +0600

    new

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get('/api', (req, res) => {
   res.json({ message: "Welcome to the iPetMyPets API!" });
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
